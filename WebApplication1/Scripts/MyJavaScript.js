985fd8cfdfe3dcd3669604e374c80f4c1fbe980f
ko22009 <ko22009@yandex.ru>
2016-04-29 23:48:29 +0500

    Init

---
+++ WebApplication1/Scripts/MyJavaScript.js
@@ -0,0 +1,6 @@
+﻿$("#target").click(function () {
+    name = $('#text_input').val();
+    count = $('#mylist div').length;
+    $('#mylist').append('<div id="' + count + '" class="list-group-item">' + name + '</div>');
+    $('#text_input').val("");
+});
