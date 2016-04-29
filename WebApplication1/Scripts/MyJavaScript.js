ce671c1a32307a086ada2119609dd5480f416ca2
ko22009 <ko22009@yandex.ru>
2016-04-30 00:06:03 +0500

    normal css

--- WebApplication1/Scripts/MyJavaScript.js
+++ WebApplication1/Scripts/MyJavaScript.js
@@ -1,6 +1,6 @@
 ﻿$("#target").click(function () {
     name = $('#text_input').val();
     count = $('#mylist div').length;
-    $('#mylist').append('<div id="' + count + '" class="list-group-item">' + name + '</div>');
+    $('#mylist').append('<div id="' + count + '" class="list-group-item">' + '<label><input type="checkbox" value="">' + name + '</label></div>');
     $('#text_input').val("");
 });
